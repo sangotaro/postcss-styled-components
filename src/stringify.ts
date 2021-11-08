50fc78cd7534d70d88b91b8cb97e562e03fc5db1
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2021-11-08 22:32:59 +0900

    refactor: remove postcss-syntax (#47)

---
+++ src/stringify.ts
@@ -0,0 +1,19 @@
+"use strict";
+
+import { Document } from "./document";
+
+function docStringify(document, builder) {
+  document.nodes.forEach((root) => {
+    builder(root.raws.beforeStart, root, "beforeStart");
+    root.source.syntax && root.source.syntax.stringify(root, builder);
+  });
+  builder(document.raws.afterEnd, document, "afterEnd");
+}
+
+export function stringify(node, builder) {
+  if (node instanceof Document) {
+    return docStringify(node, builder);
+  }
+
+  return node.source.syntax.stringify(node, builder);
+}
