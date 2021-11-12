316b5f6818fdae196c7028cd3b3ba59073961ffd
sangotaro <shotaro.watanabe0715@gmail.com>
2021-11-13 02:14:58 +0900

    refactor: extends postcss Source type

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,17 +1,23 @@
+import type { Stringifier, Builder } from "postcss";
+
 import { Document } from "./document";
 
-function docStringify(document, builder) {
+function docStringify(document: Document, builder: Builder) {
   document.nodes.forEach((root) => {
+    // @ts-expect-error TS2339: Property 'beforeStart' does not exist on type 'AtRuleRaws | RuleRaws | DeclarationRaws | CommentRaws'.
+    //Property 'beforeStart' does not exist on type 'AtRuleRaws'.
+    // TS2345: Argument of type '"beforeStart"' is not assignable to parameter of type '"start" | "end"'.
     builder(root.raws.beforeStart, root, "beforeStart");
     root.source.syntax && root.source.syntax.stringify(root, builder);
   });
+  // @ts-expect-error TS2345: Argument of type '"afterEnd"' is not assignable to parameter of type '"start" | "end"'.
   builder(document.raws.afterEnd, document, "afterEnd");
 }
 
-export function stringify(node, builder) {
+export const stringify: Stringifier = (node, builder) => {
   if (node instanceof Document) {
     return docStringify(node, builder);
   }
 
   return node.source.syntax.stringify(node, builder);
-}
+};
